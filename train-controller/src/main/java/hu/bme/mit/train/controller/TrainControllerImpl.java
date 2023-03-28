dc7e406e7a8c6fb53aa75b5c45e0555410f71dea
ruszkamuszka <ruszkamartin@gmail.com>
2023-03-28 23:53:12 +0200

    B changed

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,11 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.user.TrainUserImpl;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
-	private int referenceSpeed = 0;
+	private int referenceSpeed = 2;
 	private int speedLimit = 0;
 	private int timer = 0;
 
